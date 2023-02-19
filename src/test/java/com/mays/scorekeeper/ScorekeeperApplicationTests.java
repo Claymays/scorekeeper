4905699fd76366d5e030ed804a3a04b6fec680f1
Clayton Mays <claymays854@gmail.com>
2023-02-19 13:55:47 -0600

    adds spring boot main web initialization classes

---
+++ src/test/java/com/mays/scorekeeper/ScorekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mays.scorekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorekeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
