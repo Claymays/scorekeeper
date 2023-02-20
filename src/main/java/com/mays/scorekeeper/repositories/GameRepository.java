dbc3e9d525f8ecc3e1fea7f224facaf6d590200b
Clayton Mays <claymays854@gmail.com>
2023-02-20 17:27:53 -0600

    initializes extension for CrudRepository

--- src/main/java/com/mays/scorekeeper/repositories/GameRepository.java
+++ src/main/java/com/mays/scorekeeper/repositories/GameRepository.java
@@ -1,7 +1,10 @@
 package com.mays.scorekeeper.repositories;
 
+import com.mays.scorekeeper.entities.Game;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository {
+public interface GameRepository extends CrudRepository<Game, Integer> {
+
 }
