940cad79ef1ad2c493b3a61987a31a6d3950eff3
Clayton Mays <claymays854@gmail.com>
2023-02-20 12:43:37 -0600

    recommit from moving project location

---
+++ src/main/java/com/mays/scorekeeper/entities/Friend.java
@@ -0,0 +1,33 @@
+package com.mays.scorekeeper.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "friend")
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @ManyToOne
+    private User friendOf;
+    @ManyToMany
+    @JoinTable(name="friend_games",
+    joinColumns = @JoinColumn(name = "friend_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "game_id", referencedColumnName = "id"))
+    private List<Game> games;
+    @Column(name = "wins")
+    private float wins;
+    @Column(name = "losses")
+    private float losses;
+    @Column(name = "note")
+    private String note;
+}
