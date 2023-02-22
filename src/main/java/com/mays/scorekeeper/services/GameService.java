2dfb5d36554db06d59a621751c4ef15b407f7120
Clayton Mays <claymays854@gmail.com>
2023-02-21 18:58:06 -0600

    adds repository bean, logic for creating game record

--- src/main/java/com/mays/scorekeeper/services/GameService.java
+++ src/main/java/com/mays/scorekeeper/services/GameService.java
@@ -1,15 +1,24 @@
 package com.mays.scorekeeper.services;
 
 import com.mays.scorekeeper.entities.Game;
+import com.mays.scorekeeper.repositories.GameRepository;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Data
 @NoArgsConstructor
+@Service
 public class GameService {
 
-    public void create() {
+    @Autowired
+    GameRepository gameRepository;
 
+    public Optional<Game> create(Game game) {
+        return Optional.of(gameRepository.save(game));
     }
 
     public Game get() {
