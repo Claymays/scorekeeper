53fb313f63e40ac4f83ae1b278db7e9a50b73dd4
Clayton Mays <claymays854@gmail.com>
2023-02-21 21:27:58 -0600

    adds javadoc

--- src/main/java/com/mays/scorekeeper/services/GameService.java
+++ src/main/java/com/mays/scorekeeper/services/GameService.java
@@ -9,6 +9,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * A DAO service for Game records.
+ *
+ * @author Clayton Mays
+ */
 @Data
 @NoArgsConstructor
 @Service
@@ -17,21 +22,41 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    /**
+     * Create a game record stored in the database, and returned to caller.
+     *
+     * @param game the game
+     * @return the optional
+     */
     public Optional<Game> create(Game game) {
         return Optional.of(gameRepository.save(game));
     }
 
-    public Game get() {
-        return new Game();
+    /**
+     * Retrieves Game record from database.
+     *
+     * @param id the id to query for
+     * @return the record found
+     */
+    public Optional<Game> get(Integer id) {
+        return gameRepository.findById(id);
     }
 
-    public void update() {
-
+    /**
+     * Update a Game record in the database.
+     *
+     * @param game new version of record to be stored
+     */
+    public void update(Game game) {
+        gameRepository.save(game);
     }
 
-    public void delete() {
-
+    /**
+     * Delete a Game record.
+     *
+     * @param id query to delete
+     */
+    public void delete(Integer id) {
+        gameRepository.deleteById(id);
     }
-
-
 }
