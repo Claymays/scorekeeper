1953c82e3f81a98b2e13537dcadf41288c0ece63
Clayton Mays <claymays854@gmail.com>
2023-02-21 20:48:50 -0600

    adds complete test for creating game record

---
+++ src/test/java/com/mays/scorekeeper/services/GameServiceTest.java
@@ -0,0 +1,45 @@
+package com.mays.scorekeeper.services;
+
+import com.mays.scorekeeper.entities.Game;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DisplayName("Game Service Can:")
+class GameServiceTest {
+
+    @Autowired
+    GameService gameService;
+
+    @Test
+    @DisplayName("Can Create Game")
+    void create() {
+        Game expected = gameService.create(new Game("test")).get();
+        Game result = gameService.get(expected.getId()).get();
+        assertEquals(expected.getName(), result.getName());
+    }
+
+    @Test
+    @DisplayName("Can Retrieve Game Record")
+    void get() {
+        Game game = gameService.create(new Game("test")).get();
+
+    }
+
+    @Test
+    @DisplayName("Can Update Game Record")
+    void update() {
+        Game game = gameService.create(new Game("test")).get();
+    }
+
+    @Test
+    @DisplayName("Can Delete Game Record")
+    void delete() {
+        Game game = gameService.create(new Game("test")).get();
+    }
+}
