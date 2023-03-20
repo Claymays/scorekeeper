2b06a38e7aa0fce912e37870726e77325f766788
Clayton Mays <claymays854@gmail.com>
2023-03-20 16:21:48 -0500

    adds method for creating game object, with logic yet to be filled in.

--- src/main/java/com/mays/scorekeeper/controllers/GameController.java
+++ src/main/java/com/mays/scorekeeper/controllers/GameController.java
@@ -1,24 +1,28 @@
 package com.mays.scorekeeper.controllers;
 
+import com.mays.scorekeeper.entities.Friend;
 import com.mays.scorekeeper.entities.Game;
 import com.mays.scorekeeper.services.GameService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/game")
+@Slf4j
 public class GameController {
-    private GameService gameService;
+    private final GameService gameService;
 
     @Autowired
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
-    @PostMapping
-    public ResponseEntity create() {
-        return ResponseEntity.ok().build();
+    @PostMapping(consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+    public ResponseEntity<Game> create() {
+        return ResponseEntity.ok(new Game());
     }
 }
