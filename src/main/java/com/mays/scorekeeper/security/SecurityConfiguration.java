05ded2e525de426a8561cfe9224429aa4f9bcbf7
Clayton Mays <claymays854@gmail.com>
2023-03-26 13:20:49 -0500

    overhauls filtering logic

--- src/main/java/com/mays/scorekeeper/security/SecurityConfiguration.java
+++ src/main/java/com/mays/scorekeeper/security/SecurityConfiguration.java
@@ -1,24 +1,82 @@
 package com.mays.scorekeeper.security;
 
+import com.mays.scorekeeper.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+                        "/css/**",
+                        "/js/**",
+                        "/login",
+                        "/api/user/login",
+                        "/api/user/create"
+                )
+                .and()
+                .ignoring()
+                .antMatchers(HttpMethod.OPTIONS);
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+        http
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions().disable()
                 .and()
-                .authorizeRequests(authz -> authz.mvcMatchers("/")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .oauth2Login()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .anyRequest().anonymous()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .logout()
-                .logoutSuccessUrl("/");
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> {
+                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                    response.getWriter().write("Error");
+                });
     }
 }
