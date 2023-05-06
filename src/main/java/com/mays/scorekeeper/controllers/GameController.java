1480d532c52ac0a708f29802253c31f34f4fb981
amays2 <amays2@wisc.edu>
2023-05-06 12:15:52 -0500

    optimized imports

--- src/main/java/com/mays/scorekeeper/controllers/GameController.java
+++ src/main/java/com/mays/scorekeeper/controllers/GameController.java
@@ -1,16 +1,13 @@
 package com.mays.scorekeeper.controllers;
 
-import com.mays.scorekeeper.entities.Friend;
 import com.mays.scorekeeper.entities.Game;
 import com.mays.scorekeeper.services.GameService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 /**
