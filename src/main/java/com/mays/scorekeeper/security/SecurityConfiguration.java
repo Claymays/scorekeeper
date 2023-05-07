ef0b7e2a8271894cf2ba60cb6d9cc22fcd0fb235
amays2 <amays2@wisc.edu>
2023-05-07 16:27:20 -0500

    refactored security configuration

--- src/main/java/com/mays/scorekeeper/security/SecurityConfiguration.java
+++ src/main/java/com/mays/scorekeeper/security/SecurityConfiguration.java
@@ -1,20 +1,14 @@
 package com.mays.scorekeeper.security;
 
-import com.mays.scorekeeper.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -23,85 +17,30 @@ import org.springframework.security.web.SecurityFilterChain;
  * @author Clayton Mays
  */
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
-
-    /**
-     * Adds the user service, and password encoder to server authentication context
-     * @param auth the authentication manager being loaded into application context
-     * @throws Exception when passed properties are not configured correctly
-     */
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(userService)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    /**
-     * Password encoder bean for application
-     * @return encoder bean
-     */
+public class SecurityConfiguration  {
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth
+                        .antMatchers("/register", "/authentication").permitAll()
+                        .anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 
-    /**
-     * Creates a bean for authentication management
-     * @return Authentication manager bean
-     * @throws Exception when bean cannot be created
-     */
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    /**
-     * Configures web security to ignore resource and login paths
-     * @param web application web security context
-     */
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers(
-                        "/css/**",
-                        "/js/**",
-                        "/login",
-                        "/register",
-                        "/profile",
-                        "/api/user/login",
-                        "/api/user/"
-                )
-                .and()
-                .ignoring()
-                .antMatchers(HttpMethod.OPTIONS);
-    }
-
-    /**
-     * Catches incoming http requests, prepending jwt auth filter, and denies unauthorized resources
-     * @param http the http request
-     * @throws Exception if a nested method throws an exception
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf()
-                .disable()
-                .headers()
-                .frameOptions().disable()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/**").authenticated()
-                .anyRequest().anonymous()
-                .and().formLogin().loginPage("/login")
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                .and()
-                .logout().logoutSuccessUrl("/login")
-                .permitAll();
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("pass")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
