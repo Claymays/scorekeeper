0b7742bdf7d399d728966fd61d1a9c450fef4603
amays2 <amays2@wisc.edu>
2023-05-08 21:05:28 -0500

    added method for updating game

--- src/main/java/com/mays/scorekeeper/controllers/PageController.java
+++ src/main/java/com/mays/scorekeeper/controllers/PageController.java
@@ -1,50 +1,136 @@
 package com.mays.scorekeeper.controllers;
 
+import com.mays.scorekeeper.entities.Game;
+import com.mays.scorekeeper.entities.Team;
+import com.mays.scorekeeper.entities.User;
+import com.mays.scorekeeper.services.GameService;
 import com.mays.scorekeeper.services.UserService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
+import java.util.Optional;
 
+/**
+ * Controller for handling view and model display for application
+ *
+ * @author Clayton Mays
+ */
 @Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class PageController {
 
     private final UserService userService;
+    private final GameService gameService;
 
+    /**
+     * Static class for basic authentication/authorization transactions
+     */
     @Data
-    protected static class UserRequestBody {
+    private static class UserRequestBody {
         String username;
         String password;
     }
 
-
+    /**
+     * Access point for registration view
+     * @param model The model containing attributes of the view
+     * @return
+     */
     @GetMapping("register")
     public String register(Model model) {
         model.addAttribute("user", new UserRequestBody());
         return "register";
     }
 
+    /**
+     * Endpoint for registering a new user from form data
+     * @param newUser the user to create a record for
+     * @return a redirect to the login page
+     */
     @PostMapping("create-user")
     public String createUser(UserRequestBody newUser) {
         userService.create(newUser.getUsername(), newUser.getPassword());
         return "redirect:login";
     }
 
+    /**
+     *
+     * @return
+     */
     @GetMapping
-    public String profile(Principal principal) {
-        return "profile";
+    public String landing() {
+        return "landing";
     }
 
-    @GetMapping("newGame")
-    public String newGame(Model model, HttpServletRequest request) {
+    @GetMapping("profile")
+    public String profile() {
+        return "profile";
+    }
+    /**
+     *
+     * @param model The model containing attributes of the view
+     * @return
+     */
+    @GetMapping("new-game")
+    public String newGame(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+                String username = userDetails.getUsername();
+                Optional<User> user = userService.getByUsername(username);
+                if (user.isEmpty()) {
+                    return "redirect:error";
+                }
+                Game game = new Game(user.get());
+                game.getTeams().add(new Team(game));
+                model.addAttribute("game", game);
+                model.addAttribute(user.get());
+            }
+        }
         return "gameSetup";
+    }
+
+    /**
+     *
+     * @param model The model containing attributes of the view
+     * @param game
+     * @return
+     */
+    @PostMapping("submit-game")
+    public String submitGame(Model model, Game game) {
+        Optional<Game> savedGame = gameService.create(game);
+        if (savedGame.isEmpty()) {
+            return "redirect:error";
+        }
+        model.addAttribute(game);
+        return "game";
+    }
+
+    @RequestMapping(value="/submit-game", params={"addTeam"})
+    public String addTeam(final Game game, final BindingResult bindingResult) {
+        game.getTeams().add(new Team(game));
+        return "gameSetup";
+    }
+
+    @RequestMapping(value ="/update-game", params={"updateScores"})
+    public String updateScores(final Game game, final BindingResult bindingResult) {
+        for (Team team : game.getTeams()) {
+            team.getScores().add(team.getNewScore());
+            team.setNewScore(0);
+        }
+        gameService.update(game);
+        return "game";
     }
 }
