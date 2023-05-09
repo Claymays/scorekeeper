68070581b86d5f5a3a5c029ef4c6487b56b066a5
amays2 <amays2@wisc.edu>
2023-05-09 12:12:18 -0500

    added additional variables for team gamestate

--- src/main/java/com/mays/scorekeeper/entities/Team.java
+++ src/main/java/com/mays/scorekeeper/entities/Team.java
@@ -35,15 +35,72 @@ public class Team {
     @ManyToOne
     private Game game;
 
-    public Team(Game game) {
+    private String teamColor;
+
+    public enum BootstrapTableColor {
+        PRIMARY("table-primary"),
+        SECONDARY("table-secondary"),
+        SUCCESS("table-success"),
+        DANGER("table-danger"),
+        WARNING("table-warning"),
+        INFO("table-info"),
+        LIGHT("table-light"),
+        DARK("table-dark");
+
+        private final String cssClass;
+
+        BootstrapTableColor(String cssClass) {
+            this.cssClass = cssClass;
+        }
+
+        public String getCssClass() {
+            return cssClass;
+        }
+    }
+
+
+    public Team(Game game, int teamNumber) {
         this.scores = new ArrayList<>();
         this.name = "";
         this.game = game;
+        assignTeamColor(teamNumber);
     }
 
     public void getTotalScore() {
         for (Integer score : scores) {
             totalScore += score;
+        }
+    }
+
+    public void assignTeamColor(int teamNumber) {
+        switch (teamNumber) {
+            case 0:
+                this.teamColor = BootstrapTableColor.PRIMARY.getCssClass();
+                return;
+            case 1:
+                this.teamColor = BootstrapTableColor.SECONDARY.getCssClass();
+                return;
+            case 2:
+                this.teamColor = BootstrapTableColor.SUCCESS.getCssClass();
+                return;
+            case 3:
+                this.teamColor = BootstrapTableColor.DANGER.getCssClass();
+                return;
+            case 4:
+                this.teamColor = BootstrapTableColor.WARNING.getCssClass();
+                return;
+            case 5:
+                this.teamColor = BootstrapTableColor.INFO.getCssClass();
+                return;
+            case 6:
+                this.teamColor = BootstrapTableColor.LIGHT.getCssClass();
+                return;
+            case 7:
+                this.teamColor = BootstrapTableColor.DARK.getCssClass();
+                return;
+            default:
+                this.teamColor = "";
+                return;
         }
     }
 }
