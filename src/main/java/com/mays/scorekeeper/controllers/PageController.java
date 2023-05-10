7cf40f86e91d056a0a8ec046142958c89e913592
amays2 <amays2@wisc.edu>
2023-05-09 20:03:44 -0500

    added javadoc, profile, and landing pages

--- src/main/java/com/mays/scorekeeper/controllers/PageController.java
+++ src/main/java/com/mays/scorekeeper/controllers/PageController.java
@@ -7,13 +7,10 @@ import com.mays.scorekeeper.services.GameService;
 import com.mays.scorekeeper.services.UserService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,6 +39,20 @@ public class PageController {
     }
 
     /**
+     * Landing page for unauthenticated users.
+     *  redirect if the user is auth'ed in the session
+     * @return landing page
+     */
+    @GetMapping
+    public String landing() {
+        Optional<User> user = userService.getUserFromContext();
+        if (user.isPresent()) {
+            return "redirect:/profile";
+        }
+        return "landing";
+    }
+
+    /**
      * Access point for registration view
      * @param model The model containing attributes of the view
      * @return
@@ -57,49 +68,47 @@ public class PageController {
      * @param newUser the user to create a record for
      * @return a redirect to the login page
      */
-    @PostMapping("user/new")
+    @PostMapping("register")
     public String createUser(UserRequestBody newUser) {
         userService.create(newUser.getUsername(), newUser.getPassword());
         return "redirect:/login";
     }
 
     /**
-     *
-     * @return
+     * Returns the view for a user's profile
+     * @param model The model containing attributes of the view 
+     * @return User's profile, unless the user is not authenticated
      */
-    @GetMapping
-    public String landing() {
-        return "landing";
+    @GetMapping("profile")
+    public String profile(Model model) {
+        Optional<User> user = userService.getUserFromContext();
+        if (user.isEmpty()) {
+            return "redirect:/";
+        }
+        model.addAttribute("user", user.get());
+        return "profile";
     }
 
     /**
-     *
+     * Creates a new Game object and attaches it to the view,
+     * then returns a view for game configuration
      * @param model The model containing attributes of the view
      * @return
      */
     @GetMapping("game/new")
     public String newGame(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof UserDetails) {
-                UserDetails userDetails = (UserDetails) principal;
-                String username = userDetails.getUsername();
-                Optional<User> user = userService.getByUsername(username);
-                if (user.isEmpty()) {
-                    return "redirect:/error";
-                }
-                Game game = new Game(user.get());
-                game.getTeams().add(new Team(game, game.getTeams().size()));
-                model.addAttribute("game", game);
-                model.addAttribute(user.get());
-            }
+        Optional<User> user = userService.getUserFromContext();
+        if (user.isEmpty()) {
+            return "redirect:/login";
         }
+        Game game = new Game(user.get());
+        model.addAttribute("game", game);
+        model.addAttribute("user", user.get());
         return "gameSetup";
     }
 
     /**
-     *
+     * Endpoint that consumes a completed game form.
      * @param game form submitted game object
      * @return game view
      */
@@ -113,19 +122,46 @@ public class PageController {
         return "game";
     }
 
+    /**
+     * Form method, attaches a team to the game object.
+     * @param game form object to attach a team to.
+     * @return re-renders the game setup view with additional team
+     */
     @RequestMapping(value="/game/new/submit", params={"addTeam"})
-    public String addTeam(final Game game, final BindingResult bindingResult) {
+    public String addTeam(final Game game) {
         game.getTeams().add(new Team(game, game.getTeams().size()));
         return "gameSetup";
     }
 
+    /**
+     * Form method that updates each teams score.
+     * @param game Game object to update
+     * @return re-render the game view with updated scores
+     */
     @RequestMapping(value ="/game/update", params={"updateScores"})
-    public String updateScores(final Game game, final BindingResult bindingResult) {
+    public String updateScores(final Game game) {
         for (Team team : game.getTeams()) {
             team.getScores().add(team.getNewScore());
             team.setNewScore(0);
             team.calculateTotalScore();
         }
         return "game";
+    }
+
+    @GetMapping("game/{gameId}")
+    public String getGame(@PathVariable int gameId, Model model) {
+        model.addAttribute("game", gameService.get(gameId).get());
+        return "game";
+    }
+
+    /**
+     * Endpoint for deleting game record
+     * @param gameId id of the record to be deleted
+     * @return the user's profile page
+     */
+    @GetMapping("game/delete/{gameId}")
+    public String deleteGame(@PathVariable int gameId) {
+        gameService.delete(gameId);
+        return "redirect:/profile";
     }
 }
